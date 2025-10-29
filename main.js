38df0801f771fb7a7f8378823e8936cb92cf0cf3
Vitor Duarte Chaves <vitord.chaves@gmail.com>
2025-10-29 20:23:56 -0300

    Criação dos Tijolos

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 let ball;
 let paddle;
+let bricks = [];
+let rows = 4;
+let cols = 8;
+let brickWidth = 60;
+let brickHeight = 20;
+let spacing = 5;
 
 function setup() {
     createCanvas(600, 600);
     ball = {
         x:width/2,
         y:height/2,
-        r:10,
-        vx: random(-3,3),
-        vy: random(-3,3)
+        r: 10,
+        vx: 0,
+        vy: 0
     };
     paddle = {
         x: width / 2,
@@ -16,6 +22,7 @@ function setup() {
         w: 100,
         h: 10
     }
+    creatBriks();
 }
 
 function draw() {
@@ -28,5 +35,42 @@ function draw() {
     fill("yellow");
     rectMode(CENTER);
     rect(paddle.x, paddle.y, paddle.w, paddle.h);
-    paddle.x = constrain(mouseX, paddle.w/2, width-paddle.w/2)
+    paddle.x = constrain(mouseX, paddle.w/2, width-paddle.w/2);
+    if (ball.x - ball.r < 0 || ball.x+ ball.r > width) ball.vx *= -1;
+    if (ball.y - ball.r < 0) ball.vy *= -1;
+    if (ball.y + ball.r > paddle.y - paddle.h/2 &&
+        ball.y + ball.r > paddle.y + paddle.h/2 &&
+        ball.x > paddle.x - paddle.w/2 &&
+        ball.x < paddle.x + paddle.w/2)
+    {
+       ball.vy *= -1;
+       let diff = ball.x - paddle.x;
+       ball.vx = diff * 0.1;
+    }
+
+    for (let i = 0; i < bricks.length; i++) {
+        fill(bricks[i].color);
+        rect(bricks[i].x, bricks[i].y, bricks[i].w, bricks[i].h )
+    }
+}
+
+function mousePressed() {
+    ball.vx = random(-4, 4);
+    ball.vy = -5;
+}
+
+function creatBriks() {
+    let totalWidth = cols * (brickWidth + spacing) - spacing;
+    let startX = (width - totalWidth) - 15;
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            bricks.push({
+                x: startX + c * (brickWidth + spacing),
+                y: 80 + r * brickHeight,
+                w: brickWidth - spacing,
+                h: brickHeight - 5,
+                color: [random(100,255), random(100,255), random(100,255)]
+            });
+        }
+    }
 }
